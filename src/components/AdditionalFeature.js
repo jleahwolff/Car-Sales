413b792ceb4003d29fad2ba47e720b90faf4eb3a
Jessica Wolff <jleahwolff@gmail.com>
2020-01-22 19:14:44 -0600

    Pausing to work on yesterday's project

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -4,8 +4,8 @@ const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
-      {props.feature.name} (+{props.feature.price})
+      <button className="button" onClick={() => props.buyItem => addAction(props.features)}>Add</button>
+      {props.name} (+{props.price})
     </li>
   );
 };
